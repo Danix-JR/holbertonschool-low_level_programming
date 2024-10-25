c8328973b1d00edc7956278edde4ecf8000554a7
Danix-Jr <danihalidi10@gmail.com>
2024-10-25 11:32:22 +0200

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -15,10 +15,10 @@ int main(void)
 
 	if (n % 10 > 5)
 		printf("Last digit of %d is %d and is grater than 5\n", n, n % 10);
+	 else if (n % 10 == 0)
+		printf("Last digit of %d is %d and id 0\n", n, n  % 10);
 	else if (n % 10 < 6 && n % 10 != 0)
 		printf("Last digit of %d is %d and less tham 6 and not 0\n", n, n % 10);
-	else if (n % 10 == 0)
-		printf("Last digit of %d is %d and id 0\n", n, n  % 10);
 
 	return (0);
 }
